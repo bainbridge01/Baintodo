5d09225aa4d8f689ef2b0f690434c758c531f9ba
bainbridge01 <engr.bainbridge@gmail.com>
2020-04-09 04:58:20 +0000

    Application styling updated

--- app/controllers/baintodo_lists_controller.rb
+++ app/controllers/baintodo_lists_controller.rb
@@ -56,7 +56,7 @@ class BaintodoListsController < ApplicationController
   def destroy
     @baintodo_list.destroy
     respond_to do |format|
-      format.html { redirect_to baintodo_lists_url, notice: 'Baintodo list was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Baintodo list was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
